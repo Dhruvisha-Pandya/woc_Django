cc92a748776921286580511b3d5eb503d88fb3ef
Dhruvisha Pandya <pandyadhruvisha@gmail.com>
2025-01-22 20:47:47 +0530

    ..

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home/index.html')  # Make sure this template exists
